8024cf312e505b5215dc9ca0b0c65af5fe3315b3
kubavation <37585627+kubavation@users.noreply.github.com>
2024-01-06 19:47:05 +0100

    outbox pattern

--- src/main/java/com/durys/jakub/offerservice/events/EventsConfiguration.java
+++ src/main/java/com/durys/jakub/offerservice/events/EventsConfiguration.java
@@ -5,8 +5,10 @@ import com.durys.jakub.offerservice.events.outbox.InMemoryEventsRepository;
 import com.durys.jakub.offerservice.events.outbox.OutboxEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
+@EnableScheduling
 class EventsConfiguration {
 
     @Bean
--- src/main/java/com/durys/jakub/offerservice/events/outbox/OutboxService.java
+++ src/main/java/com/durys/jakub/offerservice/events/outbox/OutboxService.java
@@ -1,15 +1,31 @@
 package com.durys.jakub.offerservice.events.outbox;
 
+import com.durys.jakub.offerservice.events.DomainEvent;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 class OutboxService {
 
     private final EventsRepository eventsRepository;
+    private final ApplicationEventPublisher applicationEventPublisher;
 
-    OutboxService(EventsRepository eventsRepository) {
+    OutboxService(EventsRepository eventsRepository, ApplicationEventPublisher applicationEventPublisher) {
         this.eventsRepository = eventsRepository;
+        this.applicationEventPublisher = applicationEventPublisher;
     }
 
+
+    @Scheduled(cron = "*/2 * * * * *")
+    public void sendEvents() {
+
+        List<DomainEvent> events = eventsRepository.loadPending();
+
+        events
+            .forEach(applicationEventPublisher::publishEvent);
+    }
 
 }
