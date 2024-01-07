68f5e2fe14a5b29202771bd7cb1b633aad7d29e0
kubavation <37585627+kubavation@users.noreply.github.com>
2024-01-07 19:15:00 +0100

    DefaultRebatePolicyTest

---
+++ src/test/java/com/durys/jakub/offerservice/client/domain/DefaultRebatePolicyTest.java
@@ -0,0 +1,29 @@
+package com.durys.jakub.offerservice.client.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DefaultRebatePolicyTest {
+
+    DefaultRebatePolicy rebatePolicy = new DefaultRebatePolicy();
+
+    @Test
+    void shouldCalculateRebateForVipClient() {
+
+        Rebate rebate = rebatePolicy.calculateRebate(Client.Type.Vip);
+
+        assertEquals(BigDecimal.valueOf(20), rebate.amount());
+    }
+
+    @Test
+    void shouldCalculateRebateForRegularClient() {
+
+        Rebate rebate = rebatePolicy.calculateRebate(Client.Type.Regular);
+
+        assertEquals(BigDecimal.valueOf(10), rebate.amount());
+    }
+
+}
