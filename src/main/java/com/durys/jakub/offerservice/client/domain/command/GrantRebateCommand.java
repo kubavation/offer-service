39f8d9b05d7defaf27756d2e29d64c2ef59d7bcc
kubavation <37585627+kubavation@users.noreply.github.com>
2024-01-08 18:31:29 +0100

    little fix

--- src/main/java/com/durys/jakub/offerservice/client/domain/command/GrantRebateCommand.java
+++ src/main/java/com/durys/jakub/offerservice/client/domain/command/GrantRebateCommand.java
@@ -1,11 +1,12 @@
 package com.durys.jakub.offerservice.client.domain.command;
 
 import com.durys.jakub.offerservice.client.domain.ClientId;
+import com.durys.jakub.offerservice.cqrs.Command;
 
 import java.math.BigDecimal;
 import java.util.Objects;
 
-public record GrantRebateCommand(ClientId clientId, BigDecimal amount) {
+public record GrantRebateCommand(ClientId clientId, BigDecimal amount) implements Command {
 
     public GrantRebateCommand(ClientId clientId) {
         this(clientId, null);
--- src/main/java/com/durys/jakub/offerservice/client/domain/command/RemoveRebateCommand.java
+++ src/main/java/com/durys/jakub/offerservice/client/domain/command/RemoveRebateCommand.java
@@ -1,8 +1,9 @@
 package com.durys.jakub.offerservice.client.domain.command;
 
 import com.durys.jakub.offerservice.client.domain.ClientId;
+import com.durys.jakub.offerservice.cqrs.Command;
 
 import java.util.UUID;
 
-public record RemoveRebateCommand(ClientId clientId, UUID rebateId) {
+public record RemoveRebateCommand(ClientId clientId, UUID rebateId) implements Command {
 }
