d56347186480a00d96c95336954d86e456086f50
kubavation <37585627+kubavation@users.noreply.github.com>
2024-01-08 19:18:06 +0100

    init offerId domain

---
+++ src/main/java/com/durys/jakub/offerservice/offer/Offer.java
@@ -0,0 +1,14 @@
+package com.durys.jakub.offerservice.offer;
+
+import com.durys.jakub.offerservice.ddd.AggregateRoot;
+import com.durys.jakub.offerservice.events.EventPublisher;
+
+public class Offer extends AggregateRoot {
+
+    private final OfferId offerId;
+
+    Offer(EventPublisher eventPublisher, OfferId offerId) {
+        super(eventPublisher);
+        this.offerId = offerId;
+    }
+}
---
+++ src/main/java/com/durys/jakub/offerservice/offer/OfferId.java
@@ -0,0 +1,16 @@
+package com.durys.jakub.offerservice.offer;
+
+import com.durys.jakub.offerservice.common.DomainValidationException;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public record OfferId(UUID value) {
+
+    public OfferId {
+        if (Objects.isNull(value)) {
+            throw new DomainValidationException("Offer ID value cannot be empty");
+        }
+    }
+
+}
