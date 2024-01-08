1cc706582fd9489b188a239ee3b0075a981678f5
kubavation <37585627+kubavation@users.noreply.github.com>
2024-01-08 20:02:31 +0100

    offer details + factory

--- src/main/java/com/durys/jakub/offerservice/offer/domain/Offer.java
+++ src/main/java/com/durys/jakub/offerservice/offer/domain/Offer.java
@@ -2,13 +2,31 @@ package com.durys.jakub.offerservice.offer.domain;
 
 import com.durys.jakub.offerservice.ddd.AggregateRoot;
 import com.durys.jakub.offerservice.events.EventPublisher;
+import com.durys.jakub.offerservice.subsystem.SubsystemCode;
+
+import java.util.UUID;
 
 public class Offer extends AggregateRoot {
 
-    private final OfferId offerId;
+    public enum State {
+        Submitted, Published, Removed
+    }
 
-    Offer(EventPublisher eventPublisher, OfferId offerId) {
+    private final OfferId offerId;
+    private final SubsystemCode subsystemCode;
+    private Details details;
+    private State state;
+
+    Offer(EventPublisher eventPublisher, OfferId offerId, SubsystemCode subsystemCode) {
         super(eventPublisher);
         this.offerId = offerId;
+        this.subsystemCode = subsystemCode;
+    }
+
+    Offer(String name, String description, SubsystemCode subsystemCode, EventPublisher eventPublisher) {
+        super(eventPublisher);
+        this.offerId = new OfferId(UUID.randomUUID());
+        this.details = new Details(name, description);
+        this.subsystemCode = subsystemCode;
     }
 }
